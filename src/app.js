8d876d47685e89257fcdfa819473d795265adc77
Pooja Shiroya <poojashiroya99@gmail.com>
2020-12-11 20:08:27 +0530

    change

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
 
 const express = require('express');
 const app = express()
-
-
+const subscribers=require('./models/subscribers').subscriberSchema;
+const data=require('./data.js');
+app.use(express.json());
 // Your code goes here
-
-
-
-
-
-
-
-
-
+app.get('/subscribers',(req,res)=>{
+    res.send(data);
+})
+app.get('/subscribers/names',(req,res)=>{
+    res.send(data);
+})
+app.get('/subscribers/:id',(req,res)=>{
+    const id=req.params.id;
+    let user=data.indexOf((el)=>el.id===id);
+    if(!user){
+        res.status(404).send({message: error.message});
+    }
+    res.send(user);
+})
+app.listen(3000,()=>console.log("listening.."));
 
 
 
