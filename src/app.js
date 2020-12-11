19b31923ed44a206bbfe0143681b5c9b92793edc
pooja shiroya <67506850+Poojashiroya2199@users.noreply.github.com>
2020-12-11 21:07:25 +0530

    a

--- src/app.js
+++ src/app.js
@@ -1,23 +1,28 @@
 
 const express = require('express');
 const app = express()
-const subscribers=require('./models/subscribers').subscriberSchema;
-const data=require('./data.js');
+const subscribers=require('./models/subscribers');
+const ObjectId = require("mongoose");
 app.use(express.json());
 // Your code goes here
 app.get('/subscribers',(req,res)=>{
-    res.send(data);
+    subscribers.find().then(subscriber=>res.send(subscriber));
+    return;
 })
 app.get('/subscribers/names',(req,res)=>{
-    res.send(data);
+       Subscriber.find().select({name: 1, subscribedChannel: 1}).then(subscribers => res.send(subscribers));
+    return;
 })
 app.get('/subscribers/:id',(req,res)=>{
-    const id=req.params.id;
-    let user=data.indexOf((el)=>el.id===id);
-    if(!user){
-        res.status(404).send({message: error.message});
-    }
-    res.send(user);
+//     const id=req.params.id;
+//     let user=data.indexOf((el)=>el.id===id);
+//     if(!user){
+//         res.status(404).send({message: error.message});
+//     }
+//     res.send(user);
+       const id = req.params.id;
+    Subscriber.find({_id : id}).then(subscribers => subscribers.map(subscribers => res.send(subscribers))).catch(error => res.status(400).send({message: error.message}));
+    return;
 })
 app.listen(3000,()=>console.log("listening.."));
 
